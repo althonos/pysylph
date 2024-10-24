7f0da007fcf140629dbb0266d56107af68888955
Martin Larralde <martin.larralde@embl.de>
2024-10-24 22:56:19 +0200

    Add unit test for sketching a short genomic sequence

--- src/pysylph/tests/__init__.py
+++ src/pysylph/tests/__init__.py
@@ -1,9 +1,11 @@
 from . import (
     test_database,
-    test_doctest
+    test_doctest,
+    test_sketcher
 )
 
 def load_tests(loader, suite, pattern):
     suite.addTests(loader.loadTestsFromModule(test_database))
     suite.addTests(loader.loadTestsFromModule(test_doctest))
+    suite.addTests(loader.loadTestsFromModule(test_sketcher))
     return suite
